f6f0ce14989a9b4b2d42e7db07bae43e1a72a1b6
puroaman6606 <aman482004@gmail.com>
2025-09-26 17:34:15 +0530

    Fix achivementController error for Render

--- routes/achievementRoutes.js
+++ routes/achievementRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
-const { getAchievements } = require("../controllers/AchievementController");
+const { getAchievements } = require("../controllers/achievementController");
 
 // Get achievements (protected)
 router.get("/", authMiddleware, getAchievements);
