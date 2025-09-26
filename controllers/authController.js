53b89a0aafac109fbb0818d54941fdbb74172479
puroaman6606 <aman482004@gmail.com>
2025-09-26 17:10:58 +0530

    Fix user error for Render

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) throw new Error("JWT_SECRET not defined");
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 // controllers/userController.js
-import User from "../models/User.js";
+import User from "../models/user.js";
 import nodemailer from "nodemailer";
 import bcrypt from "bcryptjs";
 
